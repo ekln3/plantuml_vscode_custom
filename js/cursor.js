795b84106e1a8fa3fcbe2c0be52f1980b9bae62e
gnyaw <96630445+ekln3@users.noreply.github.com>
2022-03-06 23:23:05 +0900

    first

---
+++ js/cursor.js
@@ -0,0 +1,27 @@
+function addCursorManager() {
+    return;
+    let imgContainer= document.getElementById('image-container')
+    imgContainer.addEventListener("mousedown", e => {
+        // console.log(e);
+        //if (e.button == 1) document.body.style.cursor = "move";
+    });
+    imgContainer.addEventListener("mousemove", e => {
+        // console.log(e);
+        if (e.button == 2) return;
+        if (e.buttons == 0) {
+            if (e.altKey)
+            imgContainer.style.cursor = "zoom-out";
+            else
+            imgContainer.style.cursor = "zoom-in";
+        }
+    });
+    imgContainer.addEventListener("mouseup", e => {
+        // console.log(e);
+        if (e.button == 2) {
+            if (e.altKey)
+                imgContainer.style.cursor = "zoom-out";
+            else
+                imgContainer.style.cursor = "zoom-in";
+        }
+    });
+};
